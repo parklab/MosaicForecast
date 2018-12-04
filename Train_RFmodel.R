71bb52f831252ec53c293f4f1b465ccefd7afd5e
douym <douyanmei@gmail.com>
2018-12-04 17:57:34 -0500

    update

---
+++ Train_RFmodel.R
@@ -0,0 +1,67 @@
+#!/usr/bin/env Rscript
+.libPaths( c( .libPaths(), "/n/data1/hms/dbmi/park/yanmei/tools/R_packages/") )
+
+args = commandArgs(trailingOnly=TRUE)
+
+if (length(args)!=3) {
+	stop("Rscript Train_RFmodel.R trainset prediction_model type(Phase|Refine)
+	The \"Phase\" model indicates the RF model trained on phasing (hap=2, hap=3, hap>3); 
+	The \"Refine\" model indicates the RF model trained on Refined-genotypes from the multinomial logistic regression model (het, mosaic, repeat, refhom)
+", call.=FALSE)
+} else if (length(args)==3) {
+	input_file <- args[1]
+	prediction_model <- args[2]
+	type <- as.character(args[3])
+}
+
+library(caret)
+library(e1071)
+
+if (type=="Phase") {
+	#head demo/test_phasingcorrected
+	#id      dp_p    querypos_p      leftpos_p       seqpos_p        mapq_p  baseq_p baseq_t ref_baseq1b_p   ref_baseq1b_t   alt_baseq1b_p   alt_baseq1b_t   sb_p    context major_mismatches_mean   minor_mismatches_mean       mismatches_p    AF      dp      mapq_difference sb_read12_p     dp_diff mosaic_likelihood       het_likelihood  refhom_likelihood       althom_likelihood       phase   validation      pc1pc2      pc3     pc4     phase_corrected
+	
+	input <- read.delim(input_file, header=TRUE)
+	input$mapq_p[is.na(input$mapq_p)]<-1
+	all_train <- input
+	all_train <- subset(input, phase != "notphased")
+	all_train$phase <- as.factor(as.character(all_train$phase))
+	all_train <-all_train[!is.na(all_train$mosaic_likelihood),]
+	#all_train.2 <- subset(all_train, select=-c(althom_likelihood, id, validation, dp_p, pc1, pc2, pc3, pc4, phase))
+	#all_train.2 <- subset(all_train, select=c(querypos_p,leftpos_p,seqpos_p,mapq_p,baseq_p,baseq_t,ref_baseq1b_p,ref_baseq1b_t,alt_baseq1b_p,alt_baseq1b_t,sb_p,context,major_mismatches_mean,minor_mismatches_mean,mismatches_p,AF,dp,mapq_difference,sb_read12_p,dp_diff,mosaic_likelihood,het_likelihood,refhom_likelihood,phasing))
+	all_train.2 <- subset(all_train, select=c(querypos_p,leftpos_p,seqpos_p,mapq_p,baseq_p,baseq_t,ref_baseq1b_p,ref_baseq1b_t,alt_baseq1b_p,alt_baseq1b_t,sb_p,context,major_mismatches_mean,minor_mismatches_mean,mismatches_p,AF,dp,mapq_difference,sb_read12_p,dp_diff,mosaic_likelihood,het_likelihood,refhom_likelihood,phase))
+	
+	control <- trainControl(method="repeatedcv", number=10, repeats=3, search="grid")
+	tunegrid <- expand.grid(.mtry=30)
+	metric <- "Accuracy"
+	rf_gridsearch <- train(phase ~., data=all_train.2, method="rf", metric=metric,tuneGrid=tunegrid, trControl=control,na.action=na.exclude)
+	saveRDS(rf_gridsearch,file=prediction_model)
+	#input$prediction_phasing <- predict(rf_gridsearch, input)
+	#write.table(input, "test.prediction",sep="\t",quote=FALSE,row.names=FALSE, col.names=TRUE)
+} else if (type=="Refine"){
+	input <- read.delim(input_file, header=TRUE)
+	input$mapq_p[is.na(input$mapq_p)]<-1
+	all_train <- input
+	all_train <- subset(input, phase != "notphased")
+	all_train <-all_train[!is.na(all_train$mosaic_likelihood),]
+	#if(sum(all_train$MAF==".")>0){
+	#        all_train$MAF<-0
+	#}
+	#all_train$MAF[is.na(all_train$MAF)]<-0
+	all_train.2 <- subset(all_train, select=c(querypos_p,leftpos_p,seqpos_p,mapq_p,baseq_p,baseq_t,ref_baseq1b_p,ref_baseq1b_t,alt_baseq1b_p,alt_baseq1b_t,sb_p,context,major_mismatches_mean,minor_mismatches_mean,mismatches_p,AF,dp,mapq_difference,sb_read12_p,dp_diff,mosaic_likelihood,het_likelihood,refhom_likelihood,phase_model_corrected,conflict_num))
+	#all_train.2 <- subset(all_train, select=c(querypos_p,leftpos_p,seqpos_p,mapq_p,baseq_p,baseq_t,ref_baseq1b_p,ref_baseq1b_t,alt_baseq1b_p,alt_baseq1b_t,sb_p,context,major_mismatches_mean,minor_mismatches_mean,mismatches_p,AF,dp,mapq_difference,sb_read12_p,dp_diff,mosaic_likelihood,het_likelihood,refhom_likelihood,phase_corrected,MAF,repeats,ECNT,HCNT))
+	
+	all_train.2$sb_p[all_train.2$sb_p=="Inf"]<- 100
+	all_train.2$sb_read12_p[all_train.2$sb_read12_p=="Inf"]<- 100
+	
+	control <- trainControl(method="repeatedcv", number=10, repeats=3, search="grid")
+	tunegrid <- expand.grid(.mtry=30)
+	metric <- "Accuracy"
+	rf_gridsearch <- train(phase_model_corrected ~., data=all_train.2, method="rf", metric=metric,tuneGrid=tunegrid, trControl=control,na.action=na.exclude)
+	saveRDS(rf_gridsearch,file=prediction_model)
+
+#write.table(input, "test.prediction",sep="\t",quote=FALSE,row.names=FALSE, col.names=TRUE)
+}
+
+
+
---
+++ old/feature_extraction.effectsize.R
@@ -0,0 +1,312 @@
+#!/usr/bin/env Rscript
+.libPaths( c( .libPaths(), "/n/data1/hms/dbmi/park/yanmei/tools/R_packages/") )
+#library(coin)
+
+args<-commandArgs(TRUE)
+if (length(args)!=3) {
+  stop("Rscript feature_extraction.effectsize.R input_file(feature_list_frompython) output_file(feature_list_R) read_length(int)", call.=FALSE)
+} else if (length(args)==3) {
+	verbose=TRUE
+	input_file=args[1]
+	output_file=args[2]
+	read_length=as.numeric(args[3])
+}
+
+#id      querypos_major  querypos_minor  leftpos_major   leftpos_minor   seqpos_major    seqpos_minor    mapq_major      mapq_minor      baseq_major     baseq_minor     baseq_major_near1b      baseq_minor+near1b  major_plus      major_minus     minor_plus      minor_minus     context1        context2        context1_count  context2_count  mismatches_major        mismatches_minor        major_read1     major_read2 minor_read1     minor_read2     dp_near dp_far  dp_p
+
+input=read.delim(input_file,header=TRUE,sep=" ")
+input <- subset(input, ((((querypos_minor!="," & seqpos_minor!=",") & seqpos_major!="," )  & baseq_minor_near1b!=",") & leftpos_minor!=",") &  baseq_major_near1b!=",")
+
+my.wilcox.effectsize <- function(...) {
+	obj<-try(wilcox.test(...), silent=TRUE)
+	#if (is(obj, "try-error")) return(NA) else return(obj$statistic)
+	if (attr(obj,"class")=="try-error") return(NA) else return(obj$estimate) 
+}
+
+my.wilcox.p <- function(x)
+{
+       ref_pos=as.numeric(unlist(strsplit(as.character(x[2]),",")))
+       alt_pos=as.numeric(unlist(strsplit(as.character(x[3]),",")))
+       #round(wilcox.test(ref_pos,alt_pos,conf.int=TRUE)$estimate,5)
+	e=my.wilcox.effectsize(ref_pos,alt_pos,conf.int=TRUE)
+	if (!is.na(e)){
+		e<-e/sqrt(length(ref_pos)+length(alt_pos))
+	}
+	e
+}
+input$querypos_p=apply(input,1,my.wilcox.p)
+
+my.wilcox.p <- function(x)
+{
+       ref_pos=as.numeric(unlist(strsplit(as.character(x[4]),",")))
+       alt_pos=as.numeric(unlist(strsplit(as.character(x[5]),",")))
+       #round(wilcox.test(ref_pos,alt_pos,conf.int=TRUE)$estimate,5)
+	e=my.wilcox.effectsize(ref_pos,alt_pos,conf.int=TRUE)
+	if (!is.na(e)){
+		e<-e/sqrt(length(ref_pos)+length(alt_pos))
+	}
+	e
+}
+input$leftpos_p=apply(input,1,my.wilcox.p)
+
+my.wilcox.p <- function(x)
+{
+       ref_pos=as.numeric(unlist(strsplit(as.character(x[6]),",")))
+       alt_pos=as.numeric(unlist(strsplit(as.character(x[7]),",")))
+       #round(wilcox.test(ref_pos,alt_pos,conf.int=TRUE)$estimate,5)
+	#round(my.wilcox.effectsize(ref_pos,alt_pos,conf.int=TRUE),5)
+	e=my.wilcox.effectsize(ref_pos,alt_pos,conf.int=TRUE)
+	if (!is.na(e)){
+		e<-e/sqrt(length(ref_pos)+length(alt_pos))
+	}
+	e
+}
+input$seqpos_p=apply(input,1,my.wilcox.p)
+
+my.wilcox.p <- function(x)
+{
+       ref_mapq=as.numeric(unlist(strsplit(as.character(x[8]),",")))
+       alt_mapq=as.numeric(unlist(strsplit(as.character(x[9]),",")))
+       #round(wilcox.test(ref_mapq,alt_mapq,conf.int=TRUE)$estimate,5)
+	#round(my.wilcox.effectsize(ref_mapq,alt_mapq,conf.int=TRUE),5)
+	e=my.wilcox.effectsize(ref_mapq,alt_mapq,conf.int=TRUE)
+	if (!is.na(e)){
+		e<-e/sqrt(length(ref_mapq)+length(alt_mapq))
+	}
+	e
+}
+input$mapq_p=apply(input,1,my.wilcox.p)
+
+
+my.t.test.t.value <- function(...) {
+	obj<-try(t.test(...), silent=TRUE)
+	#if (is(obj, "try-error")) return(NA) else return(obj$statistic)
+	if (attr(obj,"class")=="try-error") return(NA) else return(obj$statistic) 
+}
+
+#my.t.test <- function(x)
+#{
+#	ref_mapq=as.numeric(unlist(strsplit(as.character(x[8]),",")))
+#	alt_mapq=as.numeric(unlist(strsplit(as.character(x[9]),",")))
+#	round(my.t.test.t.value(ref_mapq,alt_mapq),5)
+#}
+#input$mapq_t=apply(input,1,my.t.test)
+
+my.wilcox.p <- function(x)
+{
+       ref_baseq=as.numeric(unlist(strsplit(as.character(x[10]),",")))
+       alt_baseq=as.numeric(unlist(strsplit(as.character(x[11]),",")))
+       #round(wilcox.test(ref_baseq,alt_baseq,conf.input=TRUE)$estimate,5)
+	#round(my.wilcox.effectsize(ref_baseq,alt_baseq,conf.int=TRUE),5)
+	e=my.wilcox.effectsize(ref_baseq,alt_baseq,conf.int=TRUE)
+	if (!is.na(e)){
+		e<-e/sqrt(length(ref_baseq)+length(alt_baseq))
+	}
+	e
+}
+input$baseq_p=apply(input,1,my.wilcox.p)
+
+my.t.test <- function(x)
+{
+	ref_baseq=as.numeric(unlist(strsplit(as.character(x[10]),",")))
+	alt_baseq=as.numeric(unlist(strsplit(as.character(x[11]),",")))
+	round(my.t.test.t.value(ref_baseq,alt_baseq),5)	
+}
+input$baseq_t=apply(input,1,my.t.test)
+
+my.wilcox.p <- function(x)
+{
+       ref_baseq=as.numeric(unlist(strsplit(as.character(x[10]),",")))
+       ref_baseq1b=as.numeric(unlist(strsplit(as.character(x[12]),",")))
+       #round(wilcox.test(ref_baseq,ref_baseq1b,conf.int=TRUE)$estimate,5)
+	#round(my.wilcox.effectsize(ref_baseq,ref_baseq1b,conf.int=TRUE),5)
+	e=my.wilcox.effectsize(ref_baseq,ref_baseq1b,conf.int=TRUE)
+	if (!is.na(e)){
+		e<-e/sqrt(length(ref_baseq)+length(ref_baseq1b))
+	}
+	e
+	
+}
+input$ref_baseq1b_p=apply(input,1,my.wilcox.p)
+
+my.t.test <- function(x)
+{
+	ref_baseq=as.numeric(unlist(strsplit(as.character(x[10]),",")))
+	ref_baseq1b=as.numeric(unlist(strsplit(as.character(x[12]),",")))
+	round(my.t.test.t.value(ref_baseq,ref_baseq1b),5)
+}
+input$ref_baseq1b_t=apply(input,1,my.t.test)
+
+my.wilcox.p <- function(x)
+{
+       alt_baseq=as.numeric(unlist(strsplit(as.character(x[11]),",")))
+       alt_baseq1b=as.numeric(unlist(strsplit(as.character(x[13]),",")))
+       #round(wilcox.test(alt_baseq,alt_baseq1b,conf.int=TRUE)$estimate,5)
+	e=my.wilcox.effectsize(alt_baseq,alt_baseq1b,conf.int=TRUE)
+	if (!is.na(e)){
+		e<-e/sqrt(length(alt_baseq)+length(alt_baseq1b))
+	}
+	e
+}
+input$alt_baseq1b_p=apply(input,1,my.wilcox.p)
+
+my.t.test <- function(x)
+{
+	alt_baseq=as.numeric(unlist(strsplit(as.character(x[11]),",")))
+	alt_baseq1b=as.numeric(unlist(strsplit(as.character(x[13]),",")))
+	round(my.t.test.t.value(alt_baseq,alt_baseq1b),5)
+}
+input$alt_baseq1b_t=apply(input,1,my.t.test)
+
+
+my.fisher.p<- function(x)
+{
+	ref_plus <- as.numeric(unlist(as.character(x[14])))
+	ref_minus <- as.numeric(unlist(as.character(x[15])))
+	alt_plus <- as.numeric(unlist(as.character(x[16])))
+	alt_minus <- as.numeric(unlist(as.character(x[17])))
+	#round(fisher.test(cbind(c(ref_plus,ref_minus),c(alt_plus,alt_minus)))$p.value,5)
+	round(fisher.test(cbind(c(ref_plus,ref_minus),c(alt_plus,alt_minus)))$estimate,5)
+}
+input$sb_p=apply(input,1,my.fisher.p)
+
+my.context_selection <- function(x)
+{
+	if (x[20]>=x[21]) {
+	x[18]
+	} else{
+	x[19]
+	}
+}
+input$context=apply(input,1,my.context_selection)
+
+my.mean <-function(x)
+{
+       mismatch_major=as.numeric(unlist(strsplit(as.character(x[22]),",")))
+       round(mean(mismatch_major),3)
+}
+input$major_mismatches_mean=apply(input,1,my.mean)/read_length
+
+my.mean <-function(x)
+{
+       mismatch_minor=as.numeric(unlist(strsplit(as.character(x[23]),",")))
+       round(mean(mismatch_minor),3)
+}
+input$minor_mismatches_mean=apply(input,1,my.mean)/read_length
+
+my.wilcox.p <- function(x)
+{
+       mismatch_major=as.numeric(unlist(strsplit(as.character(x[22]),",")))
+       mismatch_minor=as.numeric(unlist(strsplit(as.character(x[23]),",")))-1
+       round(wilcox.test(mismatch_major,mismatch_minor,conf.int=TRUE)$estimate,5)
+}
+input$mismatches_p=apply(input,1,my.wilcox.p)
+
+my.AF <- function(x)
+{
+	depth <- sum(as.numeric(unlist(as.character(x[14]))), as.numeric(unlist(as.character(x[15]))), as.numeric(unlist(as.character(x[16]))), as.numeric(unlist(as.character(x[17]))))
+	alt <- sum(as.numeric(unlist(as.character(x[16]))), as.numeric(unlist(as.character(x[17]))))
+	round(alt/depth,3)
+}
+input$AF=apply(input,1,my.AF)
+
+my.dp <- function(x)
+{
+	depth <- sum(as.numeric(unlist(as.character(x[14]))), as.numeric(unlist(as.character(x[15]))), as.numeric(unlist(as.character(x[16]))), as.numeric(unlist(as.character(x[17]))))
+	depth
+}
+input$dp=apply(input,1,my.dp)
+
+my.mosaic_likelihood <- function(x)
+{
+	depth <- sum(as.numeric(unlist(as.character(x[14]))), as.numeric(unlist(as.character(x[15]))), as.numeric(unlist(as.character(x[16]))), as.numeric(unlist(as.character(x[17]))))
+	alt <- sum(as.numeric(unlist(as.character(x[16]))), as.numeric(unlist(as.character(x[17]))))
+	baseq_major <- as.numeric(unlist(strsplit(as.character(x[10]),",")))
+	baseq_minor <- as.numeric(unlist(strsplit(as.character(x[11]),",")))
+	r=0
+	for (i in 1: length(baseq_major)){r=r+0.1^(baseq_major[i]/10)}
+	for (i in 1: length(baseq_minor)){r=r+1-0.1^(baseq_minor[i]/10)}
+	log10(beta(r+1,depth-r+1))
+}
+input$mosaic_likelihood=apply(input,1,my.mosaic_likelihood)
+
+my.het_likelihood <- function(x)
+{
+	depth <- sum(as.numeric(unlist(as.character(x[14]))), as.numeric(unlist(as.character(x[15]))), as.numeric(unlist(as.character(x[16]))), as.numeric(unlist(as.character(x[17]))))
+	alt <- sum(as.numeric(unlist(as.character(x[16]))), as.numeric(unlist(as.character(x[17]))))
+	log10(0.5^depth)
+}
+input$het_likelihood=apply(input,1,my.het_likelihood)
+
+my.refhom_likelihood <- function(x)
+{
+	baseq_major <- as.numeric(unlist(strsplit(as.character(x[10]),",")))
+	baseq_minor <- as.numeric(unlist(strsplit(as.character(x[11]),",")))
+	q=log10(1)
+	for (i in 1: length(baseq_major)){q=q+log10(1-0.1^(baseq_major[i]/10))}
+	for (i in 1: length(baseq_minor)){q=q+log10(0.1^(baseq_minor[i]/10))}
+	q
+}
+input$refhom_likelihood=apply(input,1,my.refhom_likelihood)
+
+my.althom_likelihood <- function(x)
+{
+	baseq_major <- as.numeric(unlist(strsplit(as.character(x[10]),",")))
+	baseq_minor <- as.numeric(unlist(strsplit(as.character(x[11]),",")))
+	q=log10(1)
+	for (i in 1: length(baseq_minor)){q=q+log10(1-0.1^(baseq_minor[i]/10))}
+	for (i in 1: length(baseq_major)){q=q+log10(0.1^(baseq_major[i]/10))}
+	q
+}
+input$althom_likelihood=apply(input,1,my.althom_likelihood)
+
+my.mean <- function(x)
+{
+       ref_mapq=as.numeric(unlist(strsplit(as.character(x[8]),",")))
+       alt_mapq=as.numeric(unlist(strsplit(as.character(x[9]),",")))
+       round(mean(ref_mapq)-mean(alt_mapq),5)
+}
+input$mapq_difference=apply(input,1,my.mean)
+
+my.fisher.p<- function(x)
+{
+	ref_read1 <- as.numeric(unlist(as.character(x[24])))
+	ref_read2 <- as.numeric(unlist(as.character(x[25])))
+	alt_read1 <- as.numeric(unlist(as.character(x[26])))
+	alt_read2 <- as.numeric(unlist(as.character(x[27])))
+	#round(fisher.test(cbind(c(ref_read1,ref_read2),c(alt_read1,alt_read2)))$p.value,5)
+	round(fisher.test(cbind(c(ref_read1,ref_read2),c(alt_read1,alt_read2)))$estimate,5)
+}
+input$sb_read12_p=apply(input,1,my.fisher.p)
+
+my.diff <- function(x)
+{
+	dp_near=as.numeric(unlist(as.character(x[28])))
+	dp_far=as.numeric(unlist(as.character(x[29])))
+	dp_near-dp_far
+}
+input$dp_diff=apply(input,1,my.diff)
+
+
+output=input[,c(1,seq(30,55))]
+
+mosaic_p <- 10^(as.numeric(as.vector(output$mosaic_likelihood)))
+het_p <- 10^(as.numeric(as.vector(output$het_likelihood)))
+refhom_p <- 10^(as.numeric(as.vector(output$refhom_likelihood)))
+althom_p <- 10^(as.numeric(as.vector(output$althom_likelihood)))
+input$normalize <- mosaic_p+het_p+refhom_p+althom_p
+output$mosaic_likelihood <- mosaic_p/input$normalize
+output$het_likelihood <- het_p/input$normalize
+output$refhom_likelihood <- refhom_p/input$normalize
+output$althom_likelihood <- althom_p/input$normalize
+
+
+
+write.table(output,output_file,quote=F,sep="\t",row.names=F,col.names=T)
+
+#head -1 all_putative_mosaics_feature.list.forR
+#id      querypos_major  querypos_minor  leftpos_major   leftpos_minor   seqpos_major    seqpos_minor    mapq_major      mapq_minor      baseq_major     baseq_minor     baseq_major_near1b      baseq_minor+near1b  major_plus      major_minus     minor_plus      minor_minus     context_reference_forward       context_reference_reverse       context_antireference_forward   context_antireference_reverse   context_reference_forward_count     context_reference_reverse_count context_antireference_forward_count     context_antireference_reverse_count     mismatches_major        mismatches_minor        major_read1major_read2      minor_read1     minor_read2     dp_near dp_far  dp_p	conflict_num
+
+
+
+
