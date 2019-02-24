9e2856aeea2024eb6acd57c2e773e347fec93497
douym <douyanmei@gmail.com>
2019-02-24 18:04:29 -0500

    update

--- ReadLevel_Features_extraction.multiplethreads.py
+++ ReadLevel_Features_extraction.multiplethreads.py
@@ -2,23 +2,20 @@
 #import commands
 import os
 import sys
-import uuid
 
 program=sys.argv[0]
 arguments=sys.argv[1:]
 count=len(arguments)
-
-if count !=6:
-	print ("Usage: python(v3) ReadLevel_Features_extraction.py input_bed(file_format: chr pos-1 pos ref alt sample, sep=\"\\t\") output_features bam_dir reference_fasta Umap_mappability(bigWig file,k=24) num_threads_parallel\n\nNote:\n1. Names of bam files should be \"sample.bam\" under the bam_dir. \n\n2. There should be a fai file under the same dir of the fasta file (samtools faidx input.fa) \n\n3. We did not use gnomad population AF as an feature (instead we use it to filter), but you can use it to train your model if you have interest in common variants\n\n4. The program to extract mappability score: \"bigWigAverageOverBed\" could be downloaded here at http://hgdownload.soe.ucsc.edu/admin/exe/, the program to convert wiggle file to BigWig file \"wigToBigWig\", and the \"fetchChromSizes\" script to create the chrom.sizes file for the UCSC database with which you are working (e.g., hg19) could be downloaded from the same directory. The wiggle file containing mappability score (Umap,k=24) could be downloaded here: https://bismap.hoffmanlab.org/\n")
+if count !=5:
+	print ("Usage: python(v3) ReadLevel_Features_extraction.py input_bed(file_format: chr pos-1 pos ref alt sample, sep=\"\\t\") output_features bam_dir reference_fasta n_jobs_parallel\n\nNote: 1. name of bam files are \"sample.bam\" by default. \n2.there should be a fai file under the same dir of the fasta file (samtools faidx input.fa) \n3. we did not use dbSNP AF as an feature, but you can use it to train your model if you have interest in common variants")
 	sys.exit(1)
-elif count==6:
+elif count==5:
 	program_name = sys.argv[0]
 	input_pos=sys.argv[1] #walsh.nocluster.noalt_allele_in_normal.norepeat.bed 1       1015256 1015257 A       G       Walsh
 	output=sys.argv[2]
 	bam_dir_tmp=sys.argv[3]
 	reference_fasta=sys.argv[4]
-	unimap_mappability_BigWigfile=sys.argv[5]
-	n_jobs=sys.argv[6]
+	n_jobs=sys.argv[5]
 	#sequencing_type=sys.argv[5]
 
 import numpy as np
@@ -37,44 +34,8 @@ base['G']='C'
 base['C']='G'
 #base['N']='N'
 
-
-file=open(input_pos)
-tmp_filename=str(uuid.uuid4())
-input_mappability=open(tmp_filename,'w')
-for line in file:
-	line=line.rstrip()
-	fields=line.split('\t')
-	chr=fields[0]
-	pos=int(fields[2])
-	ref=fields[3]
-	alt=fields[4]
-	sample=fields[5]
-	ID=sample+'~'+chr+"~"+str(pos)+"~"+ref+"~"+alt
-	print("chr"+chr,pos,pos+1,ID,file=input_mappability,sep="\t")
-file.close()
-input_mappability.close()
-
-#bigWigAverageOverBed /n/data1/hms/dbmi/park/yanmei/resources/hg19/k24.umap.wg.bw test.bed test.tab
-subprocess.run("bigWigAverageOverBed "+unimap_mappability_BigWigfile+" "+tmp_filename+" "+tmp_filename+".2", shell=True, check=True)
-
-mappability=dict()
-file=open(tmp_filename+".2")
-#4638~1~86002185~G~A     1       0       0       0       0
-#4638~10~39135403~A~T    1       1       0.5     0.5     0.5
-#4638~16~21579992~C~T    1       1       0.958333        0.958333        0.958333
-for line in file:
-	line=line.rstrip()
-	fields=line.split('\t')
-	ID=fields[0]
-	score=float(fields[5])
-	mappability[ID]=score
-file.close()
-subprocess.run("rm "+tmp_filename, shell=True)
-subprocess.run("rm "+tmp_filename+".2", shell=True)
-
-
 fo=open(output,"w")
-header='id querypos_major querypos_minor leftpos_major leftpos_minor seqpos_major seqpos_minor mapq_major mapq_minor baseq_major baseq_minor baseq_major_near1b baseq_minor_near1b major_plus major_minus minor_plus minor_minus context1 context2 context1_count context2_count mismatches_major mismatches_minor major_read1 major_read2 minor_read1 minor_read2 dp_near dp_far dp_p conflict_num mappability'.split()
+header='id querypos_major querypos_minor leftpos_major leftpos_minor seqpos_major seqpos_minor mapq_major mapq_minor baseq_major baseq_minor baseq_major_near1b baseq_minor_near1b major_plus major_minus minor_plus minor_minus context1 context2 context1_count context2_count mismatches_major mismatches_minor major_read1 major_read2 minor_read1 minor_read2 dp_near dp_far dp_p conflict_num'.split()
 print (' '.join(header),file=fo)
 #print (' '.join(header))
 
@@ -295,7 +256,7 @@ def process_line(line):
 				dp_near[name].append(pileupcolumn.n)
 	
 		u, p_value = mannwhitneyu(dp_far[name], dp_near[name])
-		return name,','.join(str(x) for x in querypos_major[name])+",",  ','.join(str(x) for x in querypos_minor[name])+",",  ','.join(str(x) for x in leftpos_major[name])+",",  ','.join(str(x) for x in leftpos_minor[name])+",",  ','.join(str(x) for x in seqpos_major[name])+",",  ','.join(str(x) for x in seqpos_minor[name])+",",  ','.join(str(x) for x in mapq_major[name])+",",  ','.join(str(x) for x in mapq_minor[name])+",", ','.join(str(x) for x in baseq_major[name])+",",  ','.join(str(x) for x in baseq_minor[name])+",",  ','.join(str(x) for x in baseq_major_near1b[name])+",", ','.join(str(x) for x in baseq_minor_near1b[name])+",", major_plus[name],major_minus[name],minor_plus[name],minor_minus[name], str(context1[name]), str(context2[name]), context1_count[name],context2_count[name],','.join(str(x) for x in mismatches_major[name])+",",','.join(str(x) for x in mismatches_minor[name])+",", major_read1[name],major_read2[name],minor_read1[name],minor_read2[name],np.mean(dp_near[name]),np.mean(dp_far[name]),p_value,conflict_num[name], mappability[name]
+		return name,','.join(str(x) for x in querypos_major[name])+",",  ','.join(str(x) for x in querypos_minor[name])+",",  ','.join(str(x) for x in leftpos_major[name])+",",  ','.join(str(x) for x in leftpos_minor[name])+",",  ','.join(str(x) for x in seqpos_major[name])+",",  ','.join(str(x) for x in seqpos_minor[name])+",",  ','.join(str(x) for x in mapq_major[name])+",",  ','.join(str(x) for x in mapq_minor[name])+",", ','.join(str(x) for x in baseq_major[name])+",",  ','.join(str(x) for x in baseq_minor[name])+",",  ','.join(str(x) for x in baseq_major_near1b[name])+",", ','.join(str(x) for x in baseq_minor_near1b[name])+",", major_plus[name],major_minus[name],minor_plus[name],minor_minus[name], str(context1[name]), str(context2[name]), context1_count[name],context2_count[name],','.join(str(x) for x in mismatches_major[name])+",",','.join(str(x) for x in mismatches_minor[name])+",", major_read1[name],major_read2[name],minor_read1[name],minor_read2[name],np.mean(dp_near[name]),np.mean(dp_far[name]),p_value,conflict_num[name]
 	#print (fo)
 	except:
 		print ("context error: ",chrom, start,end)
