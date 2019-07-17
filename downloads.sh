c217b200388568c7df86233e12e86c009899a65e
douym <douyanmei@gmail.com>
2019-07-17 10:59:00 -0400

    updateREDAME

--- downloads.sh
+++ downloads.sh
@@ -17,6 +17,8 @@ chmod +x fetchChromSizes
 wget ftp://ftp-trace.ncbi.nih.gov/1000genomes/ftp/technical/reference/phase2_reference_assembly_sequence/hs37d5.fa.gz
 wget ftp://ftp-trace.ncbi.nih.gov/1000genomes/ftp/technical/reference/phase2_reference_assembly_sequence/hs37d5.fa.gz.fai -O hs37d5.fa.fai
 
-
+##repeat regions:
+wget http://hgdownload.soe.ucsc.edu/goldenPath/hg19/database/genomicSuperDups.txt.gz
+wget wget http://hgdownload.soe.ucsc.edu/goldenPath/hg19/database/simpleRepeat.txt.gz
 
 
