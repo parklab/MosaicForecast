43739b661c64f88094a0d063487476b04ad0e256
douym <douyanmei@gmail.com>
2019-10-29 22:23:06 -0400

    update prediction for phase model

--- Prediction.R
+++ Prediction.R
@@ -4,12 +4,13 @@
 
 
 args<-commandArgs(TRUE)
-if (length(args)!=3) {
-  stop("Rscript Prediction.R input_file(feature_list) model_trained output_file(predictions)", call.=FALSE)
-} else if (length(args)==3) {
+if (length(args)!=4) {
+  stop("Rscript Prediction.R input_file(feature_list) model_trained model_type(Phase|Refine) output_file(predictions)", call.=FALSE)
+} else if (length(args)==4) {
 	input_file=args[1]
 	model=args[2]
-	output_file=args[3]
+	model_type=args[3]
+	output_file=args[4]
 }
 verbose=TRUE
 
@@ -40,27 +41,50 @@ output <- subset(output,mappability>0)
 df1 <- subset(output,(type=="SNP" | type=="MNP") & indel_proportion_SNPonly<0.3)
 df2 <- subset(output, (type!="SNP" & type!="MNP") )
 output <- rbind(df1,df2)
-predictedothers <- subset(output, prediction !="mosaic")
-predictedmosaics <- subset(output, prediction=="mosaic")
-predictedmosaics <- subset(predictedmosaics, ref_softclip<0.1)
-predictedmosaics$prediction <- as.character(predictedmosaics$prediction)
 
-##optional filters (i.e., ultra-high depth):
-if (nrow(predictedmosaics[predictedmosaics$AF<0.01,])>=1){
-	predictedmosaics[predictedmosaics$AF<0.01,]$prediction <- paste0(subset(predictedmosaics,AF<0.01)$prediction,";low-confidence:AF<0.01")
+if(model_type=="Refine"){
+	predictedothers <- subset(output, prediction !="mosaic")
+	predictedmosaics <- subset(output, prediction=="mosaic")
+	predictedmosaics <- subset(predictedmosaics, ref_softclip<0.1)
+	predictedmosaics$prediction <- as.character(predictedmosaics$prediction)
+	##optional filters (i.e., ultra-high depth):
+	if (nrow(predictedmosaics[predictedmosaics$AF<0.01,])>=1){
+		predictedmosaics[predictedmosaics$AF<0.01,]$prediction <- paste0(subset(predictedmosaics,AF<0.01)$prediction,";cautious:AF<0.01")
+	}
+	if (nrow(predictedmosaics[predictedmosaics$AF*predictedmosaics$dp<2,])>=1){
+		predictedmosaics[predictedmosaics$AF*predictedmosaics$dp<2,]$prediction <- paste0(subset(predictedmosaics,AF*dp<2)$prediction,";cautious:only-1-altallele")
+	}
+	if (nrow(predictedmosaics[predictedmosaics$dp>=mean(output$dp)*2,])>=1){
+		predictedmosaics[predictedmosaics$dp>=mean(output$dp)*2,]$prediction <- paste0(subset(predictedmosaics,dp>=mean(output$dp)*2)$prediction,";low-confidence:extra-high-coverage")
+	}
+	df1 <- subset(predictedmosaics, !(dp >= mean(output$dp)*1.5 & AF>=0.2))
+	df2 <- subset(predictedmosaics, (dp >= mean(output$dp)*1.5 & AF>=0.2))
+	if(nrow(df2)>=1){
+		df2$prediction <- paste0(df2$prediction,";low-confidence:likelyCNV")
+	}
+	output <- rbind(df1,df2, predictedothers)
+}else if(model_type=="Phase"){
+	predictedothers <- subset(output, prediction !="hap=3")
+	predictedmosaics <- subset(output, prediction=="hap=3")
+	predictedmosaics <- subset(predictedmosaics, ref_softclip<0.1)
+	predictedmosaics$prediction <- as.character(predictedmosaics$prediction)
+	##optional filters (i.e., ultra-high depth):
+	if (nrow(predictedmosaics[predictedmosaics$AF<0.01,])>=1){
+		predictedmosaics[predictedmosaics$AF<0.01,]$prediction <- paste0(subset(predictedmosaics,AF<0.01)$prediction,";cautious:AF<0.01")
+	}
+	if (nrow(predictedmosaics[predictedmosaics$AF*predictedmosaics$dp<2,])>=1){
+		predictedmosaics[predictedmosaics$AF*predictedmosaics$dp<2,]$prediction <- paste0(subset(predictedmosaics,AF*dp<2)$prediction,";cautious:only-1-altallele")
+	}
+	if (nrow(predictedmosaics[predictedmosaics$dp>=mean(output$dp)*2,])>=1){
+		predictedmosaics[predictedmosaics$dp>=mean(output$dp)*2,]$prediction <- paste0(subset(predictedmosaics,dp>=mean(output$dp)*2)$prediction,";low-confidence:extra-high-coverage")
+	}
+	df1 <- subset(predictedmosaics, !(dp >= mean(output$dp)*1.5 & AF>=0.2))
+	df2 <- subset(predictedmosaics, (dp >= mean(output$dp)*1.5 & AF>=0.2))
+	if(nrow(df2)>=1){
+		df2$prediction <- paste0(df2$prediction,";low-confidence:likelyCNV")
+	}
+	output <- rbind(df1,df2, predictedothers)
 }
-if (nrow(predictedmosaics[predictedmosaics$AF*predictedmosaics$dp<2,])>=1){
-	predictedmosaics[predictedmosaics$AF*predictedmosaics$dp<2,]$prediction <- paste0(subset(predictedmosaics,AF*dp<2)$prediction,";low-confidence:only-1-altallele")
-}
-if (nrow(predictedmosaics[predictedmosaics$dp>=mean(output$dp)*2,])>=1){
-	predictedmosaics[predictedmosaics$dp>=mean(output$dp)*2,]$prediction <- paste0(subset(predictedmosaics,dp>=mean(output$dp)*2)$prediction,";low-confidence:extra-high-coverage")
-}
-df1 <- subset(predictedmosaics, !(dp >= mean(output$dp)*1.5 & AF>=0.2))
-df2 <- subset(predictedmosaics, (dp >= mean(output$dp)*1.5 & AF>=0.2))
-if(nrow(df2)>=1){
-	df2$prediction <- paste0(df2$prediction,";low-confidence:likelyCNV")
-}
-output <- rbind(df1,df2, predictedothers)
 
 write.table(output,row.names=FALSE, col.names=TRUE,sep="\t",file=output_file,quote=FALSE)
 
