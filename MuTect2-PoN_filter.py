cc7dcad3c584ce38de49b30812df22cf6d6555e0
douym <douyanmei@gmail.com>
2020-05-05 22:40:42 -0400

    add multiallelic

--- MuTect2-PoN_filter.py
+++ MuTect2-PoN_filter.py
@@ -49,7 +49,7 @@ for line in file:
 		ref_count=int(INFOs[1].split(',')[0])
 		alt_count=int(INFOs[1].split(',')[1])
 		depth=ref_count+alt_count
-		if not re.search('str_contraction', filters) and not re.search('t_lod_fstar', filters) and not re.search('triallelic_site', filters) and not re.search('panel_of_normals', filters):
+		if not re.search('str_contraction', filters) and not re.search('t_lod_fstar', filters) and not re.search('triallelic_site', filters) and not re.search('panel_of_normals', filters) and not re.search('multiallelic',filters):
 			if re.search(":0|1:", line):
 				if AF>=0.02 and AF<0.4 and alt_count>=2:
 					print(chr,pos-1,pos,ref,alt,sample,depth, AF, file=fo,sep="\t")
